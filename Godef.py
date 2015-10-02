1ecfaa29411f882e5088e5a6206eaf283a3b1891
招牌疯子 <zp@buaa.us>
2015-10-02 16:57:08 +0800

    fix some ugly codes

--- Godef.py
+++ Godef.py
@@ -10,32 +10,28 @@ class GodefCommand(sublime_plugin.WindowCommand):
     def run(self):
         print("=================[Godef]Begin=================")
         default_setting = sublime.load_settings("Preferences.sublime-settings")
-        default_line_ending = default_setting.get("default_line_ending")
-        print("[Godef]DEBUG: default_line_ending: %s" % default_line_ending)
+        # default_line_ending = default_setting.get("default_line_ending")
+        # print("[Godef]DEBUG: default_line_ending: %s" % default_line_ending)
         default_setting.set("default_line_ending", "unix")
         new_line_ending = default_setting.get("default_line_ending")
-        print("[Godef]INFO: new_line_ending: %s" % new_line_ending)
+        print("[Godef]INFO: line_ending: %s" % new_line_ending)
 
         settings = sublime.load_settings("Godef.sublime-settings")
         gopath = settings.get("gopath", os.getenv('GOPATH'))
-        if gopath is None:
+        if not gopath:
             print("[Godef]ERROR: no GOPATH defined")
             print("=================[Godef] End =================")
             return
 
         systype = platform.system()
-        print("[Godef]DEBUG: system type: %s" % systype)
+        # print("[Godef]DEBUG: system type: %s" % systype)
+        if systype == "Windows":
+            godefCmd = "godef.exe"
+        else:
+            godefCmd = "godef"
         gopaths = gopath.split(os.pathsep)
-        found = False
-        godefpath = ""
-        for path in gopaths:
-            if(systype == "Windows"):
-                godefCmd = "godef.exe"
-            else:
-                godefCmd = "godef"
-
-            godefpath = os.path.join(path, "bin", godefCmd)
-
+        for go_path in gopaths:
+            godefpath = os.path.join(go_path, "bin", godefCmd)
             if not os.path.isfile(godefpath):
                 print("[Godef]WARN: godef not found at %s" % godefpath)
                 continue
@@ -43,18 +39,14 @@ class GodefCommand(sublime_plugin.WindowCommand):
                 print("[Godef]INFO: godef found at %s" % godefpath)
                 found = True
                 break
-
         if not found:
             print("[Godef]ERROR: godef not found!")
             print("=================[Godef] End =================")
             return
-        else:
-            print("[Godef]INFO: using godef: %s" % godefpath)
+        print("[Godef]INFO: using godef: %s" % godefpath)
 
         view = self.window.active_view()
-        # row, col = view.rowcol(view.sel()[0].begin())
-        # offset = view.text_point(row, col)
-        view = self.window.active_view()
+        filename = view.file_name()
         select = view.sel()[0]
         select_begin = select.begin()
         select_before = sublime.Region(0, select_begin)
@@ -63,23 +55,13 @@ class GodefCommand(sublime_plugin.WindowCommand):
         buffer_before = bytearray(string_before, encoding="utf8")
         offset = len(buffer_before)
         print("[Godef]INFO: selcet_begin: %s offset: %s" %
-              str(select_begin),
-              str(offset))
-
-        filename = view.file_name()
-        args = [
-            godefpath,
-            "-f",
-            filename,
-            "-o",
-            str(offset)
-        ]
+              (str(select_begin), str(offset)))
+        args = [godefpath, "-f", filename, "-o", str(offset)]
         print("[Godef]INFO: spawning: %s" % " ".join(args))
-
         env = os.environ.copy()
         env["GOPATH"] = gopath
-        p = subprocess.Popen(
-            args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+        p = subprocess.Popen(args, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, env=env)
         output, stderr = p.communicate()
         if stderr:
             print("[Godef]ERROR: no definition found: %s" % str(stderr))
